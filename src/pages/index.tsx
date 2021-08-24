bb4f104662f4ac9883e1b37cbbf4b723b0b57720
unknown <gustavoc.moraes@hotmail.com>
2021-08-24 16:35:37 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>salve</h1>
+    </>
+  );
+};
+
+export default Home;
