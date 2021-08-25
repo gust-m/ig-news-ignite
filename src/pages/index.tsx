dbfbba7d3cf4038f5cc78487d2720b046bce4621
unknown <gustavoc.moraes@hotmail.com>
2021-08-25 15:02:53 -0300

    header component, signInButton component, subscribeButton component, home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
 
 const Home = (): JSX.Element => {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>salve</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 };
