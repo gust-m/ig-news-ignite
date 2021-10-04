b054dfdb84e5e4a6bf3b2e3422e41515db0dc624
unknown <gustavoc.moraes@hotmail.com>
2021-10-04 19:57:53 -0300

    Using prismic CMS, prismic API, post page

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,25 @@
+import { cloneElement, ReactElement } from 'react';
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export const ActiveLink = ({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps): JSX.Element => {
+  const { asPath } = useRouter();
+  const className = asPath === rest.href ? activeClassName : '';
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+};
